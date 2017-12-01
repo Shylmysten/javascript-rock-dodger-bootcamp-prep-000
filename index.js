b6093f5e2d08f072f452a8a50bcd3ff82e79d610
Tony Boswell <tboswell@kc.rr.com>
2017-12-01 18:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -129,8 +129,14 @@ function endGame() {
 //
 
   clearInterval(gameInterval);
-
   window.removeEventListener('keydown', moveDodger);
+  var rockLength = document.getElementById("game").getElementsByClassName("rock").length
+  var rocks = document.getElementById("game").getElementsByClassName("rock")
+  for (var i = 0; i < rockLength; i++) {
+    if (rocks[i].className == "rock") {
+      rocks[i].parentNode.removeChild(rocks[i]);
+    }
+  }
 }
 
 function moveDodger(e) {
