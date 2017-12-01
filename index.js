d024a7e6274baf1ce2e42c7ea9c74c88a187cc34
Tony Boswell <tboswell@kc.rr.com>
2017-12-01 16:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -144,15 +144,15 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   e.preventDefault();
+  
 
   if (e.which === LEFT_ARROW) {
-
+      e.preventDefault();
       moveDodgerLeft();
   }
 
   if (e.which === RIGHT_ARROW) {
-
+      e.preventDefault();
       moveDodgerRight();
   }
 
